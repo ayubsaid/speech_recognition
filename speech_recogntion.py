89c7e480d14ffaa4303dffaf9f6a783dc4e99174
ayub said <ayubayubsaid@gmail.com>
2023-05-31 13:12:39 +0500

    add several languages for recognize

--- speech_recogntion.py
+++ speech_recogntion.py
@@ -1,27 +1,20 @@
-import speech_recognition
-import pyttsx3
+import speech_recognition as sr
 
-recognizer = speech_recognition.Recognizer()
+recognizer = sr.Recognizer()
+
+languages = ["en-US", "fr-FR", "es-ES", "ru-RU", "en-GB"]  # List of language codes to recognize
 
 while True:
-
     try:
-        with speech_recognition.Microphone() as mic:
-
+        with sr.Microphone() as mic:
             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
             audio = recognizer.listen(mic)
 
-            text = recognizer.recognize_google(audio)
-            text = text.lower()
-            print(f'Recognized: {text}')
+            for lang in languages:
+                text = recognizer.recognize_google(audio, language=lang)
+                text = text.lower()
+                print(f'Recognized ({lang}): {text}')
 
-    except speech_recognition.UnknownValueError():
-
-        recognizer = speech_recognition.Recognizer()
+    except sr.UnknownValueError:
+        recognizer = sr.Recognizer()
         continue
-
-
-
-
-
-
